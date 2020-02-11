fba803fb65ecbfa7befd8468ef0d19585ac80262
Bobby Odian <odianrobert@yahoo.com>
2020-02-10 22:47:39 -0500

    added more inquirer

--- index.js
+++ index.js
@@ -25,52 +25,216 @@ inquirer.prompt ([
     {
         type: "list",
         name: "addToTracker",
-        message: "What would you like to add to the Emplyee-Tracker?",
+        message: "What would you like to do?",
         choices: [
-            "Add a department",
-            "Add a role",
+            "View all employees",
+            "View all employees by department",
+            "View all employees by manager",
             "Add an employee",
-            "View a department",
-            "View a role",
-            "View an employee",
-            "Delete a department",
-            "Delete a role",
-            "Delete an employee"
+            "Remove an employee",
+            "Update employee role",
+            "Update employee manager",
+            "View all roles",
+            "Add a role",
+            "Remove a role"
         ]
     },
 ]).then(userChoice => {
     switch(userChoice.addToTracker) {
-        case "Add a department":
+        case "View all employees":
+            viewAllEmployees();
+            break;
+        case "View all employees by department":
+            viewByDepartment();
+            break;
+        case "View all employees by manager":
+            viewByManager();
+            break;
+        case "Add an employee":
+            addEmployee();
+            break;
+        case "Add a Department":
             addDepartment();
             break;
-        case "Bid on an item":
+        case "Remove an employee":
+            removeEmployee();
+            break;
+        case "Update employee role":
+            updateEmployee();
+            break;
+        case "Update employee manager":
+            updateManager();
+            break;
+        case "View all roles":
+            viewAllRoles();
+            break;
+        case "Add a role":
             addRole();
             break;
-        case "Exit":
-            addEmployee ();
+        case "Remove a role":
+            removeRole();
+            break;
     }
 });
-function addDepartment(){
+
+function viewAllEmployees(){
+    // Display all employees;
+    addToTracker()
+};
+
+function viewByDepartment() {
+    // Display employees by department;
+    addToTracker()
+};
+
+function viewByManager() {
+    // Display employees by manager;
+    addToTracker()
+};
+
+function addEmployee() {
+    inquirer.prompt ([
+        {
+            type: "input",
+            name: "addEmployee",
+            message: "What is the employee's first name?",
+            answer: ""
+        },
+        {
+            type: "input",
+            name: "addEmployee",
+            message: "What is the employee's last name?",
+            answer: ""  
+        },
+        {
+            type: "list",
+            name: "addEmployee",
+            message: "What is the employee's role?",
+            choices: [
+                "Sales Lead",
+                "Salesperson",
+                "Lead Engineer",
+                "Software Engineer",
+                "Account Manager",
+                "Accountant",
+                "Legal Team Lead",
+                "Janitor",
+                "Copy Machine Guy"
+            ]
+        },
+        {
+            type: "input",
+            name: "addEmployee",
+            message: "Who is the employee's manager?",
+            answer: ""  
+        }
+])
+};
+
+function addDepartment() {  //adds a new column to the table
+    inquirer.prompt (
+        {
+            type: "input",
+            name: "addDepartment",
+            message: "What would you like to call the new Department?",
+            answer: ""
+        },
+    )
+};
+
+function removeEmployee() {
     inquirer.prompt ([
         {
             type: "list",
-            name: "addDepartment",
-            message: "What department would you like to add?",
+            name: "removeEmployee",
+            message: "Which employee would you like to remove?",
             choices: [
-                "engineer",
-                "developer",
-                "tester"
+                "Employee A",
+                "Employee B",
+                "Employee C",
+                "Employee D",
+                "Employee E",
+                "Employee F"
             ]
         },
     ])
-}
+};
+
+function updateEmployee() {
+    inquirer.prompt ([
+        {
+            type: "list",
+            name: "updateEmployee",
+            message: "Which employee would you like to update?",
+            choices: [
+                "Employee A",
+                "Employee B",
+                "Employee C",
+                "Employee D",
+                "Employee E",
+                "Employee F"
+            ]
+        },
+    ])
+};
+
+function updateManager() {
+    inquirer.prompt ([
+        {
+            type: "list",
+            name: "updateManager",
+            message: "Which manager would you like to update?",
+            choices: [
+                "Employee A",
+                "Employee B",
+                "Employee C",
+                "Employee D",
+                "Employee E",
+                "Employee F"
+            ]
+        },
+    ])
+};
+
+function viewAllRoles(){
+    // Display all roles;
+    addToTracker()
+};
+
+function addRole() {  //adds a new column to the table
+    inquirer.prompt (
+        {
+            type: "input",
+            name: "addRole",
+            message: "What would you like to call the new role?",
+            answer: ""
+        },
+    )
+};
+
+function removeRole() {
+    inquirer.prompt ([
+        {
+            type: "list",
+            name: "removeRole",
+            message: "Which role would you like to remove?",
+            choices: [
+                "Sales Lead",
+                "Salesperson",
+                "Lead Engineer",
+                "Software Engineer",
+                "Account Manager",
+                "Accountant",
+                "Legal Team Lead",
+                "Janitor",
+                "Copy Machine Guy"
+            ]
+        },
+    ])
+};
 
 }
-
 addToTracker()
-
-
-
 
 
 
--- seeds.sql
+++ seeds.sql
@@ -19,6 +19,6 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT(10) NOT NULL,
-    manager_id INT(10) NOT NULL,
+    manager_id INT(10),
     PRIMARY KEY(id)
 );
