e2bb85f7070637e8edda9960da6e3f27c594201f
Bobby Odian <odianrobert@yahoo.com>
2020-04-04 22:18:21 -0400

    add employee works

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mysql = require('mysql');
 //const consoleTable = require('console-table');
 const express = require('express');
 
+
 const PORT = process.env.PORT || 3000
 const app = express()
 const connection = mysql.createConnection({
@@ -28,36 +29,37 @@ inquirer.prompt ([
         message: "What would you like to do?",
         choices: [
             "View all employees",
-            "View all employees by department",
-            "View all employees by manager",
+            "View all departments",
+            "View all roles",
             "Add an employee",
-            "Remove an employee",
+            "Add a department",
+            "Add a role",
             "Update employee role",
             "Update employee manager",
-            "View all roles",
-            "Add a role",
-            "Remove a role"
+            "Remove an employee",
+            "Remove a role",
+            "View all employees by manager"
         ]
-    },
+    }
 ]).then(userChoice => {
     switch(userChoice.addToTracker) {
         case "View all employees":
-            viewAllEmployees();
+            viewAllEmployees(); //DONE
             break;
-        case "View all employees by department":
-            viewByDepartment();
+        case "View all departments":
+            viewAllDepartments(); //DONE
             break;
-        case "View all employees by manager":
-            viewByManager();
+        case "View all roles":
+            viewAllRoles();//DONE
             break;
         case "Add an employee":
-            addEmployee();
+            addEmployee();//DONE
             break;
-        case "Add a Department":
-            addDepartment();
+        case "Add a department":
+            addDepartment(); //DONE
             break;
-        case "Remove an employee":
-            removeEmployee();
+        case "Add a role":
+            addRole();//DONE
             break;
         case "Update employee role":
             updateEmployee();
@@ -65,81 +67,225 @@ inquirer.prompt ([
         case "Update employee manager":
             updateManager();
             break;
-        case "View all roles":
-            viewAllRoles();
-            break;
-        case "Add a role":
-            addRole();
+        case "Remove an employee":
+            removeEmployee();
             break;
         case "Remove a role":
             removeRole();
+            break;
+        case "View all employees by manager":
+            viewByManager(); 
             break;
     }
 });
 
 function viewAllEmployees(){
-    // Display all employees;
-    addToTracker()
-};
+    const sql = `SELECT  employee.id,first_name,last_name, role.title, department.department_name, role.salary, manager_id
+    FROM employee
+    LEFT JOIN role
+    ON employee.role_id = role.id
+    left JOIN department
+	ON department.id = role.department_id`
+    connection.query(sql, function(err, result){
+        if (err) { console.log(err) 
+        } else {
+            console.table(result)
+    }
+    addToTracker();
+});
+}; 
 
-function viewByDepartment() {
-    // Display employees by department;
+function viewAllDepartments() {
+    const sql = `SELECT  * FROM department`
+    connection.query(sql, function(err, result){
+        if (err) { console.log(err) 
+        } else {
+            console.table(result)
+    }
     addToTracker()
-};
+});
+}
 
-function viewByManager() {
-    // Display employees by manager;
-    addToTracker()
-};
-
-function addEmployee() {
-    inquirer.prompt ([
-        {
+function viewAllRoles(){
+    connection.query('SELECT role.title,department_id FROM role', function(err, result){
+      if(err) throw err;
+      console.table(result);
+      addToTracker();
+    })
+  };
+function addEmployee(result) {
+    
+        inquirer.prompt ([
+            {
             type: "input",
-            name: "addEmployee",
+            name: "first_name",
             message: "What is the employee's first name?",
-            answer: ""
+            
         },
         {
             type: "input",
-            name: "addEmployee",
+            name: "last_name",
             message: "What is the employee's last name?",
-            answer: ""  
+            
         },
         {
-            type: "list",
-            name: "addEmployee",
-            message: "What is the employee's role?",
-            choices: [
-                "Sales Lead",
-                "Salesperson",
-                "Lead Engineer",
-                "Software Engineer",
-                "Account Manager",
-                "Accountant",
-                "Legal Team Lead",
-                "Copy Machine Guy"
-            ]
+            type: "number",
+            name: "roleId",
+            message: "What is the employees role ID"
         },
         {
-            type: "input",
-            name: "addEmployee",
-            message: "Who is the employee's manager?",
-            answer: ""  
+            type: "number",
+            name: "managerId",
+            message: "What is the employees manager's ID?"
         }
-])
-};
+        // {
+            //     type: "list",
+        //     name: "addEmployeeRole",
+        //     message: "What is the employee's role?",
+        //     choices: [
+            //         "Sales Lead",
+        //         "Salesperson",
+        //         "Lead Engineer",
+        //         "Software Engineer",
+        //         "Account Manager",
+        //         "Accountant",
+        //         "Legal Team Lead",
+        //         "Copy Machine Guy"
+        //     ]
+        // },
+        // {
+        //     type: "list",
+        //     name: "addEmployeeManager",
+        //     message: "Who is the employee's manager?",
+        //     choices: [
+        //         "1",
+        //         "2",
+        //         "3",
+        //         "null"
+        //     ],
+        
+        // {
+        //     type: "list",
+        //     name: "addRoleId",
+        //     choices: role.id.map(role => {
+        //         role.title
+        //         console.log(role.title);
+        //     })
+        // },
+    ]).then(function (answer) {
+        console.log(answer);
+  
+        var query = `INSERT INTO employee SET ?`
+        connection.query(query,
+          {
+            first_name: answer.first_name,
+            last_name: answer.last_name,
+            role_id: answer.roleId,
+            manager_id: answer.managerId,
+          },
+          function (err, res) {
+            if (err) throw err;
+  
+            console.table(res);
+            console.log("Inserted successfully!\n");
+  
+            addToTracker();
+          });
+      });
+  }
+//     ]).then(({ addFirstName, addLastName, roleID,addEmployeeManager }) => {
+//     roleID = process.argv[2]; 
+//     // let empId;
+//     // result.map(finds => {
+//         // if(finds.title === addRoleId) {
+//         //   empId = finds.id;
+//     connection.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES(?,?,?,?)',
 
-function addDepartment() {  //adds a new column to the table
+//         { 
+//             first_name: addFirstName, 
+//             last_name: addLastName,
+//             role_id: roleID,
+//             manager_id: addEmployeeManager
+//         }),
+//         console.log(addEmployeeManager);
+//         console.log(`\n Success! You added a new employee: with first name of ${addFirstName}, \n With a last name of ${addLastName}`)
+//         addToTracker()
+//     }
+//     //  }
+//     )
+// //  })    
+// };
+
+function addDepartment() {
     inquirer.prompt (
         {
             type: "input",
             name: "addDepartment",
-            message: "What would you like to call the new Department?",
+            message: "What department would you like to add?",
+            answer: ""
+        }
+    ).then(({ addDepartment}) => {
+        connection.query('INSERT INTO department SET ?', {
+            name: addDepartment
+      },
+
+      console.log(`\n Success! You added a new department: ${addDepartment}`)
+      );
+
+      connection.query('SELECT * FROM department', function(err, result) {
+        if(err) throw err;
+        console.table(result);
+        addToTracker()
+    });
+})      
+};
+
+function addRole() {  //adds a new column to the table
+    connection.query('SELECT * FROM department', function(err, result){
+        if(err) throw err;
+    inquirer.prompt ([
+        {
+            type: "input",
+            name: "addTitle",
+            message: "What would you like to call the new title?",
             answer: ""
         },
-    )
-};
+        {
+            type: "input",
+            name: "addSalary",
+            message: "What is the salary(in numerical value) for the new title?",
+            answer: "",
+        validate: function (addSalary) {
+            if (isNaN(addSalary) === false){
+              return true;
+            }
+            return 'Please enter a numerical salary';
+          }
+        },
+        {
+            type: "list",
+            name: "addId",
+            choices: result.map(department => department.department_name)
+        },
+    ]).then(({ addTitle, addSalary, addId }) => {
+        let deptId;
+        result.map(finds => {
+            if(finds.department_name === addId) {
+              deptId = finds.id;
+        connection.query('INSERT INTO role SET ?', 
+            { 
+            title: addTitle, 
+            salary: addSalary,
+            department_id: deptId
+            },
+            console.log(`\n Success! You added a new job title: ${addTitle}, \n With a salary of ${addSalary}`)
+            );
+            addToTracker()
+          }
+        })
+    });
+})
+}
 
 function removeEmployee() {
     inquirer.prompt ([
@@ -157,58 +303,6 @@ function removeEmployee() {
             ]
         },
     ])
-};
-
-function updateEmployee() {
-    inquirer.prompt ([
-        {
-            type: "list",
-            name: "updateEmployee",
-            message: "Which employee would you like to update?",
-            choices: [
-                "Employee A",
-                "Employee B",
-                "Employee C",
-                "Employee D",
-                "Employee E",
-                "Employee F"
-            ]
-        },
-    ])
-};
-
-function updateManager() {
-    inquirer.prompt ([
-        {
-            type: "list",
-            name: "updateManager",
-            message: "Which manager would you like to update?",
-            choices: [
-                "Employee A",
-                "Employee B",
-                "Employee C",
-                "Employee D",
-                "Employee E",
-                "Employee F"
-            ]
-        },
-    ])
-};
-
-function viewAllRoles(){
-    // Display all roles;
-    addToTracker()
-};
-
-function addRole() {  //adds a new column to the table
-    inquirer.prompt (
-        {
-            type: "input",
-            name: "addRole",
-            message: "What would you like to call the new role?",
-            answer: ""
-        },
-    )
 };
 
 function removeRole() {
@@ -232,25 +326,45 @@ function removeRole() {
     ])
 };
 
+function updateEmployee() {
+    inquirer.prompt ([
+        {
+            type: "list",
+            name: "updateEmployee",
+            message: "Which employee would you like to update?",
+            // choices: filter.map(role => role.title)
+        },
+    ])
+};
+
+function updateManager() {
+    inquirer.prompt ([
+        {
+            type: "list",
+            name: "updateManager",
+            message: "Which manager would you like to update?",
+            choices: [
+                "Employee A",
+                "Employee B",
+                "Employee C",
+                "Employee D",
+                "Employee E",
+                "Employee F"
+            ]
+        },
+    ])
+};
+
+function viewByManager() {
+    // Display employees by manager;
+    addToTracker()
+};
+
 }
+
 addToTracker()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(PORT, () => {
     console.log('Server listening on: http://localhost:${PORT}')
 })
+
--- seeds.sql
+++ seeds.sql
@@ -1,71 +1,104 @@
 USE employeeTracker_DB;
 
-CREATE TABLE department (
-    id INT(10) AUTO_INCREMENT,
-    name VARCHAR(30),
-    PRIMARY KEY(id)
-);
+-- CREATE TABLE department (
+--     id INT(10) AUTO_INCREMENT,
+--     name VARCHAR(30),
+--     PRIMARY KEY(id)
+-- );
 
-CREATE TABLE role (
-    id INT(10) AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL(10,2) NOT NULL,
-    department_id INT(10) NOT NULL,
-    PRIMARY KEY(id)
-);
+-- CREATE TABLE role (
+--     id INT(10) AUTO_INCREMENT,
+--     title VARCHAR(30) NOT NULL,
+--     salary DECIMAL(10,2) NOT NULL,
+--     department_id INT(10) NOT NULL,
+--        CONSTRAINT fk_department
+-- 	   FOREIGN KEY(department_id)
+-- 	   	REFERENCES department(id),
+--     PRIMARY KEY(id)
+-- );
 
-CREATE TABLE employee (
-    id INT(10) AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT(10) NOT NULL,
-    manager_id INT(10),
-    PRIMARY KEY(id)
-);
-
-
-INSERT INTO department (name) 
-VALUES ("Sales"),
-	   ("Sales"),
-	   ("Engineering"),
-	   ("Engineering"),
-	   ("Accounting"),
-	   ("Accounting"),
-	   ("Legal"),
-	   ("Office");
-       
-Select * FROM department;	   
+-- CREATE TABLE employee (
+--     id INT(10) AUTO_INCREMENT,
+--     first_name VARCHAR(30) NOT NULL,
+--     last_name VARCHAR(30) NOT NULL,
+--     role_id INT(10) NOT NULL,
+--        CONSTRAINT fk_role
+-- 	   FOREIGN KEY(role_id)
+-- 	   	REFERENCES role(id),
+--     manager_id INT(10),
+--        CONSTRAINT fk_roles
+-- 	   FOREIGN KEY(manager_id)
+-- 	   	REFERENCES employee(id),
+--     PRIMARY KEY(id)
+-- );
 
 
-INSERT INTO role (title, salary, department_id)
-VALUES ("Sales Lead", 150000, 2000),
-	   ("Salesperson", 90000, 2000),
-	   ("Lead Engineer", 175000, 1000),
-	   ("Software Engineer", 125000, 1000),
-	   ("Account Manager", 150000, 3000),
-	   ("Accountant", 100000, 3000),
-	   ("Legal Team Lead", 200000, 4000),
-	   ("Copy Machine Guy", 12000, 9000);
-
-Select * FROM role;
+-- INSERT INTO department (name) 
+-- VALUES ("Sales"),
+-- 	   ("Engineering"),
+-- 	   ("Accounting"),
+-- 	   ("Legal"),
+-- 	   ("Office");
+--        
+-- Select * FROM department;	   
 
 
+-- INSERT INTO role (title, salary, department_id)
+-- VALUES ("Sales Lead", 150000, 1),
+-- 	   ("Salesperson", 90000, 1),
+-- 	   ("Lead Engineer", 175000, 2),
+-- 	   ("Software Engineer", 125000, 2),
+-- 	   ("Account Manager", 150000, 3),
+-- 	   ("Accountant", 100000, 3),
+-- 	   ("Legal Team Lead", 200000, 4),
+-- 	   ("Copy Machine Guy", 12000, 5);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Jack", "Torrance", 221, 1001),
-	   ("Leia", "Organa", 222, 2001),
-	   ("Tony", "Montana", 111, null),
-	   ("Bonnie", "Parker", 112, 1001),
-	   ("John", "Blutarsky", 333, null),
-	   ("Peggy", "Olson", 334, 3001),
-	   ("Rocky", "Balboa", 444, null),
-	   ("Rachel", "Green", 999, 1001);
+-- Select * FROM role;
 
-Select * FROM employee;       
 
-                
-                
-         
+
+-- INSERT INTO employee (first_name, last_name, role_id, manager_id)
+-- VALUES ("Jack", "Torrance", 1, 1),
+-- 	   ("Leia", "Organa", 2, 2),
+-- 	   ("Tony", "Montana", 3, null),
+-- 	   ("Bonnie", "Parker", 4, 1),
+-- 	   ("John", "Blutarsky", 5, null),
+-- 	   ("Peggy", "Olson", 6, 3),
+-- 	   ("Rocky", "Balboa", 7, null),
+-- 	   ("Rachel", "Green", 8, 1);
+
+-- Select * FROM employee;       
+
+-- SELECT  employee.id,first_name,last_name, role.title, department.department_name, role.salary, manager_id
+--     FROM employee
+--     LEFT JOIN role
+--     ON employee.role_id = role.id
+--     left JOIN department
+-- 	ON department.id = role.department_id
+
+SELECT employee.first_name,last_name,  
+
+-- ALTER TABLE department RENAME COLUMN name TO department_name;
+
+
+    
+
+    
+    
+-- Persons (PersonID, Name, SS)*E
+-- Fears (FearID, Fear)*D
+-- Person_Fear (ID, PersonID, FearID)*R
+
+-- Select Persons.Name, Persons.SS, Fears.Fear
+-- From Persons
+-- LEFT JOIN Persons_Fear
+-- ON Persons.PersonID = Person_Fear.PersonID
+-- LEFT JOIN Fears
+-- ON Person_Fear.FearID = Fears.FearID;
+-- emp dep dep rol
+
+
+
                 
                 
                 
